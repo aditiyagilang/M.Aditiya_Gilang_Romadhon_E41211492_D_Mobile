8351ee95770b93ded58075eee87ffb3baf05d560
aditiyagilang <aditiyagilang031@gmail.com>
2023-02-16 13:42:28 +0700

    Minggu 2

--- Minggu2/app/src/main/java/com/aditiyagilang/minggu2/framelayout.java
+++ Minggu2/app/src/main/java/com/aditiyagilang/minggu2/framelayout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class customsize extends AppCompatActivity {
+public class framelayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customsize);
+        setContentView(R.layout.activity_framelayout);
     }
 }

83d03cf93d5c0944f8b5fa698636a582cde63b91
aditiyagilang <aditiyagilang031@gmail.com>
2023-02-27 20:00:50 +0700

    minggu3

---
+++ Minggu3/acara_152/app/src/main/java/com/aditiyagilang/acara_15/MahasiswaAdapter.java
@@ -0,0 +1,46 @@
+package com.aditiyagilang.acara_15;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.MahasiswaViewHolder> {
+
+    private ArrayList<Mahasiswa> dataList;
+
+    public MahasiswaAdapter(ArrayList<Mahasiswa> dataList) {
+        this.dataList = dataList;
+    }
+
+    @Override
+    public MahasiswaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.activity_card_layout, parent, false);
+        return new MahasiswaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MahasiswaViewHolder holder, int position) {
+        holder.txtNama.setText(dataList.get(position).getNama());
+        holder.txtNpm.setText(dataList.get(position).getNpm());
+        holder.txtNohp.setText(dataList.get(position).getNohp());
+    }
+    @Override
+    public int getItemCount() {
+        return (dataList != null) ? dataList.size() : 0;
+    }
+    public class MahasiswaViewHolder extends RecyclerView.ViewHolder{
+        private TextView txtNama, txtNpm, txtNohp;
+        public MahasiswaViewHolder(View itemView) {
+            super(itemView);
+            txtNama = (TextView) itemView.findViewById(R.id.txt_nama_mahasiswa);
+            txtNpm = (TextView) itemView.findViewById(R.id.txt_npm_mahasiswa);
+            txtNohp = (TextView) itemView.findViewById(R.id.txt_Nohp_mahasiswa);
+        }
+    }
+}
--- Minggu3/acara_152/app/src/main/java/com/aditiyagilang/acara_15/MainActivity.java
+++ Minggu3/acara_152/app/src/main/java/com/aditiyagilang/acara_15/MainActivity.java
@@ -1,14 +1,38 @@
 package com.aditiyagilang.acara_15;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private MahasiswaAdapter adapter;
+    private RecyclerView recyclerView;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addData();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+    void addData(){
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Aditiya Gilang", "E41211492", "089509579032"));
+        mahasiswaArrayList.add(new Mahasiswa("Reyhan Febriansyah", "E41211491", "082229474403"));
+        mahasiswaArrayList.add(new Mahasiswa("Wishal Azharyan ", "E41211570", "082232937743"));
+        mahasiswaArrayList.add(new Mahasiswa("Lusy Damayanti", "E41211262", "089503415644"));
+        mahasiswaArrayList.add(new Mahasiswa("Karen Novita", "E41211595", "08386467271"));
+        mahasiswaArrayList.add(new Mahasiswa("Andru Cristo", "E41211466", "085232398005"));
     }
 }

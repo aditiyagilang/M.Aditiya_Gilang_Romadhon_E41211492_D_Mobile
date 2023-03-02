ff905563431359c278ace1adeb1bd6f67b41eaf4
aditiyagilang <aditiyagilang031@gmail.com>
2023-03-02 15:00:36 +0700

    minggu4

--- Minggu4/acara18/app/src/main/java/com/aditiyagilang/acara18/MainActivity.java
+++ Minggu4/acara18/app/src/main/java/com/aditiyagilang/acara18/MainActivity.java
@@ -1,14 +1,41 @@
 package com.aditiyagilang.acara18;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn1, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+btn1 = findViewById(R.id.firstfragment);
+btn2 = findViewById(R.id.secundfragment);
+
+btn1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        load(new fragment());
     }
+});
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                load(new fragment2());
+            }
+        });
+
+}
+private void load(Fragment fragment){
+    FragmentManager fragmentManager =    getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.frame,fragment);
+    fragmentTransaction.commit();
+}
 }
---
+++ Minggu4/acara18/app/src/main/java/com/aditiyagilang/acara18/fragment1.java
@@ -0,0 +1,14 @@
+package com.aditiyagilang.acara18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment1);
+    }
+}
